161794931292ef158a58d3f07705a7b3e213e185
niteshmishra <nmishra4@ncsu.edu>
2023-05-11 01:51:48 -0400

    Add Task functionality on Button

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import {useState} from 'react'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false)
 
   const [tasks, setTasks] = useState([
     {
@@ -15,7 +17,7 @@ function App() {
       id:2,
     text: 'Univ Assignment ',
     day: 'Feb 10',
-    reminder: true},
+    reminder: false},
     {
       id:3,
     text: 'Meeting Appointment',
@@ -23,16 +25,35 @@ function App() {
     reminder: false}
   ])
 
+  // Add Task
+  const addTask = (task) => {
+    console.log(task)
+    const id = Math.floor(Math.random() * 10000)+1
+    const newTask = {id, ...task}
+    setTasks([...tasks, newTask])
+
+  }
+
   //Delete Task 
   const deleteTask = (id) => {
     console.log('Deleted Task:', id)
     setTasks(tasks.filter((task) => task.id !== id))
   }
 
+  // Toggle remainder
+  // Changing the reminder value to its opposite (true-false) on double click
+  const toggleReminder = (id) => {
+    console.log('Reminder Task:', id)
+    setTasks(tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task))
+  }
+
   return (
     <div className="container">
-      <Header />
-      <Tasks tasks={tasks} onDelete={deleteTask} />
+      <Header onAdd={() => setShowAddTask(!showAddTask)} />
+      
+      {showAddTask && <AddTask onAdd={addTask} /> }
+      
+      {tasks.length >0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle = {toggleReminder} /> : ('No tasks')}
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,17 @@
 import Button  from "./Button"
-const Header = ({ title }) => {
+const Header = ({ title, onAdd }) => {
 
   //Acting as an event for our button
-  const onClick = () => {
-    console.log('onClick')
-  }
+  // const onClick = () => {
+  //   console.log('onClick')
+  // }
+
+  // the onAdd function shows and hind the form on click.
   return (
     <header className='header'>
         <h1 > Task Tracker {title}</h1>
 
-        <Button color='steelblue' text='Add' onClick={onClick}/> 
+        <Button color='steelblue' text='Add' onClick={onAdd}/> 
     </header>
   )
 }
